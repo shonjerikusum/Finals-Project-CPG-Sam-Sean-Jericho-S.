02a7d675cabe641bd6120836f17113d3789c468d
shonjerikusum <samseanjericho@gmail.com>
2024-05-19 13:41:42 +0800

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,9 @@
+function scrollToHome() {
+    document.getElementById('bahay').scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToActivities() {
+    document.getElementById('background1').scrollIntoView({ behavior: 'smooth' });
+}
+
+
