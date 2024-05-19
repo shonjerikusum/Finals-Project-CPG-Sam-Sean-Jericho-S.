d5697f5e4f0e419390ab7266fbacfdd35fdd65c3
shonjerikusum <samseanjericho@gmail.com>
2024-05-19 20:53:03 +0800

    Delete JavaScript directory

--- JavaScript/functions.js
+++
@@ -1,9 +0,0 @@
-function scrollToHome() {
-    document.getElementById('bahay').scrollIntoView({ behavior: 'smooth' });
-}
-
-function scrollToActivities() {
-    document.getElementById('background1').scrollIntoView({ behavior: 'smooth' });
-}
-
-
