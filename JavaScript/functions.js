caf55aa8a9afa8ce6f547b1118b4ed93c08dfd2a
shonjerikusum <samseanjericho@gmail.com>
2024-05-19 20:54:46 +0800

    Add files via upload

---
+++ JavaScript/functions.js
@@ -0,0 +1,13 @@
+function scrollToHome() {
+    document.getElementById('bahay').scrollIntoView({ behavior: 'smooth', block:'center' });
+}
+
+function scrollToActivities() {
+    document.getElementById('gawain').scrollIntoView({ behavior: 'smooth', block:'center' });
+}
+
+function scrollToContacts() {
+    document.getElementById('tawag').scrollIntoView({behavior: 'smooth', block:'center'});
+}
+
+
